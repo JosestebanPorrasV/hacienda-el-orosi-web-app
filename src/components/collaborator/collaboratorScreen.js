b1d369050608c2c1cdf5f7de9a0a13b1ed9bb3d0
elvinlab <60045534+elvinlab@users.noreply.github.com>
2021-02-10 21:14:49 -0600

    creando rutas principales

---
+++ src/components/collaborator/collaboratorScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const collaboratorScreen = () => {
+    return (
+        <div>
+            <h1>Este es el modulo de colaboradores</h1>
+        </div>
+    )
+}
---
+++ src/components/lend/LendScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const LendScreen = () => {
+    return (
+        <div>
+            <h1>Esto es el modulo de prestamo</h1>
+        </div>
+    )
+}

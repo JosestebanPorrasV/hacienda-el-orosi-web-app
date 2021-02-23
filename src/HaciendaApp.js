e6561ddf520bfc463eb57b15dbb19c4654baf19c
advargar <adrian.vargar22@gmail.com>
2021-02-23 14:56:23 -0600

    Falta agregar/quitar hidden sidebar

--- src/HaciendaApp.js
+++ src/HaciendaApp.js
@@ -3,12 +3,11 @@ import React from "react";
 import { AppRouter } from "./routers/AppRouter";
 import { Provider } from "react-redux";
 import { Store } from "./store/Store";
-import { Dashboard } from "./components/ui/Dashboard";
 
 export const HaciendaApp = () => {
     return (
         <Provider store = { Store } >
-            <Dashboard />
+            <AppRouter />
         </Provider>
     )
 }

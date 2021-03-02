784f661ac320f497268a38cdffb1470ba2919c9b
JosestebanPorrasV <60045487+JosestebanPorrasV@users.noreply.github.com>
2021-03-02 17:50:29 -0600

    tool, action  and screen

--- src/components/tool/ToolScreen.js
+++ src/components/tool/ToolScreen.js
@@ -1,30 +1,31 @@
 import React,  { useEffect, useState } from "react";
 import  { useDispatch } from " react-redux ";
 import { useSelector } from " react-redux ";
-import { paymentStartLoadign } from "../../actions/PaymentAction";
+import { toolStartLoadign } from "../../actions/ToolAction";
 
-export const PaymentScreen = () => {
+export const ToolScreen = () => {
 const dispatch = useDispatch();
-const { payments, count } = useSelector ((state) => state.payment);
+const { tools, count } = useSelector ((state) => state.tool);
 
 useEffect(() => {
-    dispatch(paymentStartLoading(1));
+    dispatch(tooltStartLoading(1));
 },[dispatch]);
 return(
     <div className = "main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
     <div className="bg-gradient-to-b from-gray-300 to-gray-100 uppercase text-gray-800 border-b-2 border-gray-300 rounded-tl-lg rounded-tr-lg p-5">
-    <h5 className="font-bold uppercase text-gray-600">Pagos realizados</h5>
+    <h5 className="font-bold uppercase text-gray-600">Herramientas Registradas</h5>
     </div>
     <div className = "p-5">
    <table className="w-full p-5 text-gray-700">
        <thead className="justify-between">
        <tr className="bg-gray-800 text-gray-300">
-           <th>Pago Realizado Por el Administrador</th>
-           <th>Colaborador</th>
-           <th className="px-16 py-2"> Fecha de pago</th>
-           <th className="px-16 py-2"> Salario neto</th>
-           <th className="px-16 py-2"> Salario final</th>
-           <th className="px-16 py-2"> Detalles</th>
+           <th>Herramienta Registrada por el Administrador</th>
+           <th>Herramienta</th>
+           <th className="px-16 py-2"> active_num</th>
+           <th className="px-16 py-2"> date</th>
+           <th className="px-16 py-2"> name</th>
+           <th className="px-16 py-2"> status</th>
+           <th className="px-16 py-2"> liters</th>
        </tr>
        </thead>
        <tbody className = "bg-gay-200">
--- src/routers/DashboardRoutes.js
+++ src/routers/DashboardRoutes.js
@@ -9,6 +9,7 @@ import { CollaboratorScreen } from "../components/collaborator/CollaboratorScree
 import { AdministratorScreen } from "../components/administrator/AdministratorScreen";
 import { LendScreen } from "../components/lend/LendScreen";
 import { ContractScreen } from "../components/contract/ContractScreen";
+import { ToolScreen } from "../components/tool/ToolScreen";
 export const DashboardRoutes = () => {
   return (
     <Router>
@@ -26,6 +27,7 @@ export const DashboardRoutes = () => {
         <Route exact path="/listar-prestamos" component={LendScreen} />
         <Route exact path="/listar-contratos" component={ContractScreen} />
         <Redirect to="/listar-colaboradores" />
+        <Route exact path="/listar-herramientas" component={ToolScreen} />
       </Switch>
     </Router>
   );

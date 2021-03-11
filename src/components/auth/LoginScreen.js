946f851e0333aa5cd9ba9c8b97bd1850d8afcd72
elvinlab <60045534+elvinlab@users.noreply.github.com>
2021-03-10 20:42:59 -0600

    mejoras

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -30,7 +30,7 @@ export const LoginScreen = () => {
     >
       <div className="flex flex-col items-center flex-1 h-full justify-center px-4 sm:px-0">
         <div
-          className="flex rounded-lg shadow-lg w-full sm:w-3/4 lg:w-1/2 bg-black bg-opacity-50  sm:mx-0"
+          className="flex rounded-lg shadow-2xl w-full sm:w-3/4 lg:w-1/2 bg-black bg-opacity-50  sm:mx-0"
           style={{ height: "500px" }}
         >
           <div className="flex flex-col w-full md:w-1/2 p-4">

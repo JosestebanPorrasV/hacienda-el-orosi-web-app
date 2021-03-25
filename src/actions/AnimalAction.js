6af74153fa2d0c2a6d647508fc356f14868f5f7d
elvinlab <60045534+elvinlab@users.noreply.github.com>
2021-03-25 00:51:44 -0600

    Listando ganado estabulado

---
+++ src/actions/AnimalAction.js
@@ -0,0 +1,30 @@
+import { Types } from "../types/Types";
+import { FetchConsult } from "../helpers/FetchService";
+import Swal from "sweetalert2";
+import TopLoaderService from "top-loader-service";
+
+export const animalsByTypeLoading = (type = "Ganado estabulado", page = 1) => {
+  return async (dispatch) => {
+    await TopLoaderService.start();
+    try {
+      const resp = await FetchConsult(`gestion-animal/tipo/${type}/${page}`);
+
+      const body = await resp.json();
+      
+      if (body.status) {
+        await dispatch(animalsLoaded(body.animals));
+        await TopLoaderService.end();
+      } else {
+        await Swal.fire("Error", body.msg, "error");
+        await TopLoaderService.end();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+const animalsLoaded = (animals) => ({
+  type: Types.ANIMALS_LOADED,
+  payload: animals,
+});

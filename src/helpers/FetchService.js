b89b978ef65f60d16d482d9f44bf3f5360be0f97
Elvin González <60045534+elvinlab@users.noreply.github.com>
2021-04-07 19:33:30 -0600

    firebase

--- src/helpers/FetchService.js
+++ src/helpers/FetchService.js
@@ -3,16 +3,21 @@ const baseUrl = process.env.REACT_APP_API_URL;
 const FetchConsult = (endpoint, data, method = "GET") => {
   const url = `${baseUrl}/${endpoint}`;
   const token = localStorage.getItem("token") || "";
-  
-    return fetch(url, {
-      method,
-      headers: {
-        "Content-type": "application/json",
-        Authorization: token,
-      },
-      body: JSON.stringify(data),
+
+  return fetch(url, {
+    method: method,
+    headers: {
+      "Content-type": "application/json",
+      Authorization: token,
+    },
+    body: JSON.stringify(data),
+  })
+    .then(function (response) {
+      return response;
+    })
+    .catch(function (err) {
+      console.error(err);
     });
-  
 };
 
 export { FetchConsult };

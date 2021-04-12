ef373a55487283dd0cc12de1dcdd4d88ec587275
advargar <adrian.vargar22@gmail.com>
2021-04-11 20:40:01 -0600

    Some modifications

--- src/components/contract/ContractScreen.js
+++ src/components/contract/ContractScreen.js
@@ -11,39 +11,125 @@ export const ContractScreen = () => {
     dispatch(contractsLoading());
   }, [dispatch]);
   return (
-    <div className="main-content flex-1 bg-gray-100 mt-20 md:mt-2 pb-5 md:pb-5">
-      <div className="bg-green-900 pt-20">
-        <div className="rounded-tl-3xl bg-gradient-to-r from-blue-900 to-gray-800 p-4 shadow text-2xl text-white">
-          <h3 className="font-bold pl-2">Contratos Activos</h3>
-        </div>
+    <>
+    <div className="bg-gradient-to-r from-yellow-200 rounded-lg px-4 lg:px-8 py-4 lg:py-6 mt-8 flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-12">
+      <div>
+        <h2 className="text-2xl text-yellow-900">CONTRATOS</h2>
+        <p className="text-yellow-900 opacity-70">
+          Funcionalidades principales
+        </p>
       </div>
-      <div className="p-5">
-        <table className="w-full p-5 text-gray-700 border-separate">
-          <thead className="justify-between">
-            <tr className="bg-gray-800 text-gray-300">
-            <th className="px-16 py-2"> Contratado </th>
-              <th className="px-16 py-2"> Fecha de contrato </th>
-              <th className="px-16 py-2"> Dia de pago </th>
-              <th className="px-16 py-2"> Precio </th>
-              <th className="px-16 py-2"> Telefono de contacto </th>
-            </tr>
-          </thead>
-          <tbody className="bg-gray-200">
-            {contracts.map((item, index) => (
-              <tr
-                key={index}
-                className="bg-white border-4 border-gray-200 text-center ml-2 font-semibold"
-              >
-                <th className="px-16 py-2">{item.name_contracted}</th>
-                <th className="px-16 py-2">{item.date_contract}</th>
-                <th className="px-16 py-2">{item.date_pay}</th>
-                <th className="px-16 py-2">{item.amount}</th>
-                <th className="px-16 py-2">{item.number_phone}</th>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+      <nav className="md:flex md:space-x-4 space-y-2 md:space-y-0 text-lg text-gray-200">
+        <button
+          className="inline-flex flex-col justify-center items-center m-1 px-3 py-3 bg-black rounded-lg hover:bg-gray-800 w-35 fas fa-hand-holding-usd"
+        >
+          <span>Añadir Contrato</span>
+        </button>
+        <button
+          onClick={() => dispatch(contractsLoading("Activo"))}
+          className="inline-flex flex-col justify-center items-center m-1 px-3 py-3 bg-black  rounded-lg hover:bg-gray-800 w-35 fas fa-chart-line"
+        >
+          <span>Listar contratos activos</span>
+        </button>
+        <button
+          onClick={() => dispatch(contractsLoading("Cancelado"))}
+          className="inline-flex flex-col justify-center items-center m-1 px-3 py-3 bg-black  rounded-lg hover:bg-gray-800 w-35 fas fa-strikethrough"
+        >
+          <span>Listar contratos terminados</span>
+        </button>
+      </nav>
     </div>
+    {contracts.length !== 0 ? (
+      <>
+      
+        <div className="bg-gray-700 rounded-lg px-4 lg:px-8 py-4 lg:py-6 mt-8 ">
+          <h2
+            className="text-green-400text-xl font-bold mb-2"
+          >
+          Activos</h2>
+
+          <span
+            className="bg-green-200 text-green-600 md:ml-2 py-1 px-1 rounded-t-lg  inline-block text-center uppercase"
+          >
+            <i className="fas fa-box-open"></i> {`total: ${count}`}
+          </span>
+          <div className="overflow-x-auto">
+            <div className="align-middle inline-block min-w-full overflow-hidden">
+              <SearchResults
+                value={filter}
+                data={contracts}
+                renderResults={(results) => (
+                  <table className="min-w-full">
+                    <thead className="bg-gray-600">
+                      <tr className="bg-gray-600 text-white text-lg">
+                        <th className="py-2 px-3">
+                          <i className="fas fa-signal"></i> Estado
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-user"></i> Colaborador
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-id-card"></i> Cedula
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-calendar-day"></i> Registrado
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-funnel-dollar"></i> Monto
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-comments-dollar"></i> Cuota
+                          semanal
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-dollar-sign"></i> Restante
+                        </th>
+                        <th className="py-2 px-3">
+                          <i className="fas fa-cash-register"></i> Cuotas
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-blue-100 text-opacity-80 whitespace-nowrap">
+                      {results.map((contract) => (
+                        <tr key={contract._id}>
+                          
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                )}
+              />
+            </div>
+          </div>
+        </div>
+        <ReactPaginate
+          pageCount={Math.ceil(count / 10)}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={2}
+          previousLabel={"Atras"}
+          activeClassName={"bg-green-900 rounded-full my-1"}
+          breakClassName={"text-2xl text-grey-900 pl-4"}
+          nextLabel={"Adelante"}
+          breakLabel={"..."}
+          pageLinkClassName={
+            "flex items-center px-4 py-2 mx-1 text-white text-bold transition-colors duration-200 transform bg-gray-900 rounded-full my-1"
+          }
+          previousClassName={
+            "flex items-center px-4 py-2 mx-1 text-white text-bold transition-colors duration-200 transform bg-green-700 rounded-full hover:bg-green-900"
+          }
+          nextClassName={
+            "flex items-center px-4 py-2 mx-1 text-white text-bold transition-colors duration-200 transform bg-green-700 rounded-full hover:bg-green-900"
+          }
+      
+          containerClassName={"sm:flex m-4 p-3"}
+        />
+      </>
+    ) : (
+      <span className="ml-2 text-gray-400 whitespace-nowrap italic">
+        - ( No se encontraron contratos ) -
+      </span>
+    )}
+
+  </>
   );
 };

09fd770ad413badac1177e98242f91c772035265
advargar <adrian.vargar22@gmail.com>
2021-05-20 23:21:21 -0600

    Medicament ready

--- src/actions/MedicamentAction.js
+++ src/actions/MedicamentAction.js
@@ -3,6 +3,60 @@ import { FetchConsult } from '../helpers/FetchService';
 import Swal from 'sweetalert2';
 import TopLoaderService from 'top-loader-service';
 
+export const MedicamentsLoaded = () => {
+  return async (dispatch) => {
+    await TopLoaderService.start();
+    try {
+      const resp = await FetchConsult(`gestion-salud//listar-medicamentos`);
+      const body = await resp.json();
+
+      if (body.status) {
+        await dispatch(medicamentsLoaded(body));
+
+        await TopLoaderService.end();
+      } else {
+        await Swal.fire("Error", body.msg, "error");
+
+        await TopLoaderService.end();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export function saveMedicament(medicamentFormValues) {
+  return async (dispatch) => {
+    await TopLoaderService.start();
+    const resp = await FetchConsult(
+      "gestion-salud/guardar-medicamento",
+      {
+        name: medicamentFormValues.name,
+        quantity: medicamentFormValues.quantity,
+        milliliters: medicamentFormValues.milliliters,
+        unit_price: medicamentFormValues.unit_price,
+      },
+      "POST"
+    );
+
+    const body = await resp.json();
+    if (body.status) {
+      await dispatch(addMedicamentSuccess(body));
+      await Swal.fire({
+        icon: "success",
+        title: body.msg,
+        showConfirmButton: false,
+        timer: 2000,
+      });
+
+      await TopLoaderService.end();
+    } else {
+      await Swal.fire("Error", body.msg, "error");
+      await TopLoaderService.end();
+    }
+  };
+}
+
 export const searchMedicament = (medicamentName) => {
   return async (dispatch) => {
     await TopLoaderService.start();
@@ -30,6 +84,37 @@ export const searchMedicament = (medicamentName) => {
   };
 };
 
+export const medicamentDelete = ({ _id }) => {
+  return async (dispatch) => {
+    await TopLoaderService.start();
+    try {
+      const resp = await FetchConsult(
+        `gestion-salud/remover-medicamento/${_id}`,
+        { },
+        "DELETE"
+      );
+
+      const body = await resp.json();
+      if (body.status) {
+        await Swal.fire("Eliminado", body.msg, "success");
+
+        await dispatch(deleteOneMedicament());
+
+        await TopLoaderService.end();
+      } else {
+        await Swal.fire("Error", body.msg, "error");
+        await TopLoaderService.end();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+export const deleteOneMedicament = () => ({
+  type: Types.DELETE_MEDICAMENT,
+});
+
 export const medicamentSetActive = (medicament) => ({
   type: Types.MEDICAMENT_SET_ACTIVE,
   payload: medicament
@@ -38,7 +123,12 @@ export const medicamentClearActive = () => ({
   type: Types.MEDICAMENT_CLEAR_ACTIVE
 });
 
+export const addMedicamentSuccess = (medicament) => ({
+  type: Types.ADD_MEDICAMENT,
+  payload: medicament,
+});
+
 const medicamentsLoaded = (medicaments) => ({
-  type: Types.PRODUCTS_LOADED,
+  type: Types.MEDICAMENTS_LOADED,
   payload: medicaments
 });
